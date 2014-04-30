de5b90b7b6ec7f1eef48a5f1a22b31723b627812
Pablo Torres <tn.pablo@gmail.com>
2014-04-29 20:12:06 -0400

    get ALL the data

--- gitScript.js
+++ gitScript.js
@@ -1,21 +1,22 @@
 var https = require('https');
 var gitHubbard = require('github');
 var async = require('async');
+var _ = require('underscore');
 
 var github = new gitHubbard({
   version: "3.0.0",
   protocol: "https",
-  debug: true,
+  // debug: true,
   host: "api.github.com",
   timeout: 5000
 });
 github.authenticate({
-  type: "basic",
-  username: "ptn",
+  type: "oauth",
+  token: "",
 });
 
 // Pulled from the Hacker School API.
-var userList = [
+var hsers = [
   "davidbalbert",
   "zachallaun",
   "rose",
@@ -81,23 +82,119 @@ var userList = [
   "bwigoder"
 ];
 
-var repos =[];
-var startDate = new Date(2014, 1, 9);
+var repos = {};
+var startDate = new Date(2014, 1, 10);
 
-async.eachSeries(userList, function(user, nextUser) {
+function getCommits(repoName, owner, cbk) {
+  var payload = { user: owner, repo: repoName };
+  var cleaned = [];
+  github.repos.getCommits(payload, function(err, data) {
+    if (err) { return cbk(err); }
+    async.eachSeries(data, function(commit, nextCommit) {
+      var date = new Date(commit.commit.author.date)
+      var author = null;
+      if (commit.author) {
+        author = commit.author.login;
+      }
+      if(date > startDate) {
+        cleaned.push({
+          author: author,
+          message: commit.commit.message,
+          date: date
+        });
+      }
+      nextCommit();
+    }, function(err) {
+      cbk(err, cleaned);
+    });
+  });
+};
+
+function getLanguages(repoName, owner, cbk) {
+  var payload = { user: owner, repo: repoName };
+  github.repos.getLanguages(payload, function(err, data) {
+    if (err) { return cbk(err); }
+
+    var languages = Object.keys(data);
+    var index = languages.indexOf('meta');
+    if (index > -1) {
+      languages.splice(index, 1);
+    }
+
+    cbk(err, languages);
+  });
+};
+
+function findStreak(commits) {
+  var longest = 1;
+  for (var i = 1; i < commits.length; i++) {
+    if (commits[i].date.getDay() !== commits[i-1].date.getDay()) {
+      longest += 1;
+    } else {
+      longest = 1;
+    }
+  }
+  return longest;
+};
+
+function formatRepo(raw, owner, cbk) {
+  if (raw.fork) {
+    return cbk(null);
+  }
+
+  var pushDate = new Date(raw.pushed_at);
+  if(pushDate <= startDate){
+    return cbk(null);
+  }
+
+  getCommits(raw.name, owner, function(err, commits) {
+    if (err) { return cbk(err); }
+    getLanguages(raw.name, owner, function(err, languages) {
+      if (err) { return cbk(err); }
+
+      var collabs = _.uniq(_.map(commits, function(c) { return c.author; }));
+      var days = _.uniq(_.map(commits, function(c) { return c.date; }));
+      var cleaned = {
+        name: raw.name,
+        collabs: collabs,
+        commits: commits,
+        languages: languages,
+        days: days,
+        streak: findStreak(commits),
+      };
+
+      cbk(err, cleaned);
+    });
+  });
+};
+
+function addToLanguages(repo) {
+  for (var i = 0; i < repo.languages.length; i++) {
+    if (repos[repo.languages[i]] === undefined) {
+      repos[repo.languages[i]] = [];
+    }
+    repos[repo.languages[i]].push(repo);
+  }
+};
+
+async.eachSeries(hsers, function(user, nextUser) {
   github.repos.getFromUser({user: user}, function(err, res) {
-    if(err){
+    if(err) {
       console.log('repos of user ' + user + ' could not be fetched');
+      console.log(err);
       nextUser(err);
     }
     async.eachSeries(res, function(repo, nextRepo) {
-      if (!repo.fork) {
-        var pushDate = new Date(repo.pushed_at);
-        if(pushDate > startDate){
-          repos.push(repo.full_name);
+      var repoName = repo.name;
+      formatRepo(repo, user, function(err, repo) {
+        if (err) {
+          console.log("could not clean repo " + user + "/" + repoName);
+          console.log(err);
+        } else if (repo) {
+          addToLanguages(repo);
         }
-      }
-      nextRepo();
+        nextRepo();
+      });
     }, function(err) {
       if(err) {
         console.log('error while iterating over repos of user ' + user);
@@ -111,6 +208,6 @@ async.eachSeries(userList, function(user, nextUser) {
     console.log('error while iterating over users');
     console.log(err);
   } else {
-    console.log(repos);
+    console.log(JSON.stringify(repos));
   }
 });
